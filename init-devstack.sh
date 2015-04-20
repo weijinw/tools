55e3ee12e627f55176db018323b951ebffcb61b2
Weijin Wang <wiwang@vmware.com>
2015-04-20 12:18:47 -0700

    Added script for updating hostname of the devstack box.

---
+++ init-devstack.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# Set hostname
+# ==================================
+OLD_HOSTNAME="$( hostname )"
+NEW_HOSTNAME="$1"
+
+if [ -z "$NEW_HOSTNAME" ]; then
+ echo -n "Please enter new hostname: "
+ read -e NEW_HOSTNAME
+fi
+
+if [ -z "$NEW_HOSTNAME" ]; then
+ echo "Error: no hostname entered. Exiting."
+ exit 1
+fi
+
+echo "Changing hostname from $OLD_HOSTNAME to $NEW_HOSTNAME..."
+
+hostname "$NEW_HOSTNAME"
+
+sed -i "s/HOSTNAME=.*/HOSTNAME=$NEW_HOSTNAME/g" /etc/hostname
+
+if [ -n "$( grep "$OLD_HOSTNAME" /etc/hosts )" ]; then
+ sed -i "s/$OLD_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
+else
+ echo -e "$( hostname -I | awk '{ print $1 }' )\t$NEW_HOSTNAME" >> /etc/hosts
+fi
+
+echo "Done."
